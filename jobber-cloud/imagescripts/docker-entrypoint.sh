d64f695e0ed0380cf887b75778e856e0f7aa3750
Steffen Bleul <blacklabelops@itbleul.de>
2016-01-29 01:37:34 +0100

    Added importing of gpg keys.

--- jobber-cloud/imagescripts/docker-entrypoint.sh
+++ jobber-cloud/imagescripts/docker-entrypoint.sh
@@ -2,13 +2,21 @@
 
 set -o errexit
 
+if [ -n "${CLOUD_GPG_PRIVATE_KEY}" ]; then
+  gpg --allow-secret-key-import --import ${CLOUD_GPG_PRIVATE_KEY}
+fi
+
+if [ -n "${CLOUD_GPG_PUBLIC_KEY}" ]; then
+  gpg --import ${CLOUD_GPG_PUBLIC_KEY}
+fi
+
 source /opt/cloud/environment-amazonws.sh
 source /opt/cloud/environment-gcloud.sh
 source /opt/cloud/environment-tutum.sh
 source /opt/cloud/environment-docker.sh
 
-if [ -n "${DELAYED_START}" ]; then
-  sleep ${DELAYED_START}
+if [ -n "${CLOUD_DELAYED_START}" ]; then
+  sleep ${CLOUD_DELAYED_START}
 fi
 
 /opt/jobber/docker-entrypoint.sh "$@"
