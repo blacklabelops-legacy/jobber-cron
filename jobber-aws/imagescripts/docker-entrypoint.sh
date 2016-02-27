3456770ea14cc30fbd42c9dc122fed32dcf93ce9
Steffen Bleul <sbl@itbleul.de>
2016-02-27 01:11:34 +0100

    Migrated cloud image to alpine.

---
+++ jobber-aws/imagescripts/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit
+
+if [ ! -f "/root/.gnupg/pubring.gpg" ]; then
+  if [ -n "${CLOUD_GPG_PRIVATE_KEY}" ]; then
+    gpg --allow-secret-key-import --import ${CLOUD_GPG_PRIVATE_KEY}
+  fi
+
+  if [ -n "${CLOUD_GPG_PUBLIC_KEY}" ]; then
+    gpg --import ${CLOUD_GPG_PUBLIC_KEY}
+  fi
+fi
+
+source /opt/cloud/environment-amazonws.sh
+
+if [ -n "${CLOUD_DELAYED_START}" ]; then
+  sleep ${CLOUD_DELAYED_START}
+fi
+
+/opt/jobber/docker-entrypoint.sh "$@"
