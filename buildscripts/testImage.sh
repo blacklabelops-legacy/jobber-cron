9dbb3cd8a1cc88f435f55ee3ff0659174856c04e
Steffen Bleul <sbl@itbleul.de>
2016-06-19 22:22:42 +0200

    Added branching mechanism.

--- buildscripts/testImage.sh
+++ buildscripts/testImage.sh
@@ -4,18 +4,30 @@ set -o errexit    # abort script at first error
 
 function testPrintVersion() {
   local tagname=$1
-  docker run --rm blacklabelops/jobber:$tagname jobber -v
+  local branch=$(git rev-parse --abbrev-ref HEAD)
+  if  [ "${branch}" = "master" ]; then
+    imagename=$tagname
+  else
+    imagename=$tagname-$branch
+  fi
+  docker run --rm blacklabelops/jobber:$imagename jobber -v
 }
 
 function testImage() {
   local tagname=$1
-  docker run -d --name=$tagname -e "JOB_NAME1=TestEcho" -e "JOB_COMMAND1=echo hello world" -e "JOB_TIME1=0 30 15 * * *" blacklabelops/jobber:$tagname
-  docker exec $tagname jobber list
-  docker exec $tagname jobber log
-  docker exec $tagname jobber reload
-  docker exec $tagname jobber test TestEcho
-  docker exec $tagname jobber cat TestEcho
-  docker rm -f -v $tagname
+  local branch=$(git rev-parse --abbrev-ref HEAD)
+  if  [ "${branch}" = "master" ]; then
+    imagename=$tagname
+  else
+    imagename=$tagname-$branch
+  fi
+  docker run -d --name=$imagename -e "JOB_NAME1=TestEcho" -e "JOB_COMMAND1=echo hello world" -e "JOB_TIME1=0 30 15 * * *" blacklabelops/jobber:$tagname
+  docker exec $imagename jobber list
+  docker exec $imagename jobber log
+  docker exec $imagename jobber reload
+  docker exec $imagename jobber test TestEcho
+  docker exec $imagename jobber cat TestEcho
+  docker rm -f -v $imagename
 }
 
 testPrintVersion $1
