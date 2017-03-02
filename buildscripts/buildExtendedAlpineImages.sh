4a6276a1b94bbf611604779e8652c47615863005
Steffen Bleul <sbl@itbleul.de>
2017-03-02 19:11:45 +0100

    Removing java-app console command from gcloud cli.

--- buildscripts/buildExtendedAlpineImages.sh
+++ buildscripts/buildExtendedAlpineImages.sh
@@ -14,9 +14,9 @@ source $CUR_DIR/buildExtendedImage.sh tools jobber-tools
 source $CUR_DIR/buildExtendedImage.sh tools.$BUILD_CONTAINER_VERSION jobber-tools
 source $CUR_DIR/buildExtendedImage.sh aws jobber-aws
 source $CUR_DIR/buildExtendedImage.sh aws.$BUILD_CONTAINER_VERSION jobber-aws
-#source $CUR_DIR/buildExtendedImage.sh gce jobber-gcloud
-#source $CUR_DIR/buildExtendedImage.sh gce.$BUILD_CONTAINER_VERSION jobber-gcloud
+source $CUR_DIR/buildExtendedImage.sh gce jobber-gcloud
+source $CUR_DIR/buildExtendedImage.sh gce.$BUILD_CONTAINER_VERSION jobber-gcloud
 source $CUR_DIR/buildExtendedImage.sh docker jobber-docker
 source $CUR_DIR/buildExtendedImage.sh docker.$BUILD_CONTAINER_VERSION jobber-docker
-#source $CUR_DIR/buildExtendedImage.sh cloud jobber-cloud
-#source $CUR_DIR/buildExtendedImage.sh cloud.$BUILD_CONTAINER_VERSION jobber-cloud
+source $CUR_DIR/buildExtendedImage.sh cloud jobber-cloud
+source $CUR_DIR/buildExtendedImage.sh cloud.$BUILD_CONTAINER_VERSION jobber-cloud
